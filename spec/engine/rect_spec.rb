fd090d12180a44eaabe8719115903ac38662f67e
Inclooder <inclooder@gmail.com>
2017-02-24 23:59:04 +0100

    Add tests for half_width and half_height plus fix tests readability

--- spec/engine/rect_spec.rb
+++ spec/engine/rect_spec.rb
@@ -3,53 +3,64 @@ require_relative '../../lib/engine/point'
 
 RSpec.describe Engine::Rect do
   describe '#center' do
-    subject do
-      described_class.new(5, 7, 10, 10).center
-    end
     it 'returns point' do
-      is_expected.to be_a(Engine::Point)
+      rect = Engine::Rect.new(5, 7, 10, 10)
+
+      expect(rect.center).to be_a(Engine::Point)
     end
 
     it 'returns center of rectangle' do
-      is_expected.to eq Engine::Point.new(10, 12)
+      rect = Engine::Rect.new(5, 7, 10, 10)
+
+      expect(rect.center).to eq Engine::Point.new(10, 12)
     end
   end
 
   describe '#collide_point?' do
-    subject do
-      described_class.new(43, 10, 80, 80)
-                     .collide_point?(point.x, point. y)
-    end
-    context 'when point collide' do
-      let(:point) { Engine::Point.new(50, 18) }
-      it 'returns true' do
-        is_expected.to be true
-      end
+    it 'returns true when point collide' do
+      rect = Engine::Rect.new(43, 10, 80, 80) 
+      point = Engine::Point.new(50, 18)
+
+      expect(rect.collide_point?(point.x, point.y)).to be true
     end
 
-    context 'when point not collide' do
-      let(:point) { Engine::Point.new(42, 11) }
-      it 'returns false' do
-        is_expected.to be false
-      end
+    it 'returns false when point not collide' do
+      rect = Engine::Rect.new(43, 10, 80, 80) 
+      point = Engine::Point.new(42, 11)
+
+      expect(rect.collide_point?(point.x, point.y)).to be false
     end
   end
 
   describe '#topleft' do
-    subject do
-      described_class.new(3, 2, 4, 3).topleft
-    end
     it 'returns top-left corner' do
-      is_expected.to eq Engine::Point.new(3, 2)
+      rect = Engine::Rect.new(3, 2, 4, 3)
+      
+      expect(rect.topleft).to eq Engine::Point.new(3, 2)
     end
   end
 
   describe '#bottomright' do
-    subject do
-      described_class.new(3, 2, 4, 3).bottomright
-    end
     it 'returns bottom-right corner' do
-      is_expected.to eq Engine::Point.new(7, 5)
+      rect = Engine::Rect.new(3, 2, 4, 3)
+
+      expect(rect.bottomright).to eq Engine::Point.new(7, 5)
+    end
+  end
+
+  describe '#half_width' do
+    it 'returns half of width' do
+      rect = Engine::Rect.new(0, 0, 10, 10)
+
+      expect(rect.half_width).to eq 5
+    end
+  end
+
+  describe '#half_height' do
+    it 'returns half of height' do
+      rect = Engine::Rect.new(0, 0, 10, 10)
+
+      expect(rect.half_height).to eq 5
     end
   end
 end
